c0c946cba5c6e7bc5dfa15f474e9bc12467f055f
greffmate <greffmate7898@gmail.com>
2023-11-06 11:45:23 +0100

    gyakpython elorol

---
+++ gyak.py
@@ -0,0 +1,21 @@
+x=print("Hello")
+print=(x)
+
+def fv(num):
+    if (num>5):
+        return 0
+    else:
+        print("kisebb")
+
+y=fv(3)
+print(y)
+
+print("hello\nWo\trd")
+print(len("\n"))
+
+print("hdfsk\\number"),
+
+print("eloszo," "masodikszo", end=" ")
+print("Python")
+
+print("eloszo," "masodikszo", sep=":")
